e091eabd3597d2c53c48d469a7cfd55e6ef39fd9
Leonardo Antonio Vanzella <leovanzella@gmail.com>
2013-10-04 19:38:43 -0300

    Corrigida identa??o :(

--- spi.c
+++ spi.c
@@ -2,30 +2,42 @@
 
 void spi_init(void)  
 {  
-	 SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;     					  /* Turn on clock to D module */ 
-	 SIM->SCGC4 |= SIM_SCGC4_SPI0_MASK;      					  /* Enable SPI0 clock */ 
-	 PORTD->PCR[0] = PORT_PCR_MUX(0x2);      					  /* Set PTD0 to mux 2 [SPI0_PCS0] */
-	 PORTD->PCR[1] = PORT_PCR_MUX(0x2);      					  /* Set PTD1 to mux 2 [SPI0_SCK] */
-	 PORTD->PCR[2] = PORT_PCR_MUX(0x2);      					  /* Set PTD2 to mux 2 [SPI0_MOSI] */
-	 PORTD->PCR[3] = PORT_PCR_MUX(0x2);      					  /* Set PTD3 to mux 2 [SPIO_MISO] */
-	 
-	 SPI0->C1 = SPI_C1_MSTR_MASK | SPI_C1_SSOE_MASK;    /* Set SPI0 to Master & SS pin to auto SS */
+	SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;     					  /* Turn on clock to D module */ 
+	SIM->SCGC4 |= SIM_SCGC4_SPI0_MASK;      					  /* Enable SPI0 clock */ 
+	PORTD->PCR[0] = PORT_PCR_MUX(0x2);      					  /* Set PTD0 to mux 2 [SPI0_PCS0] */
+	PORTD->PCR[1] = PORT_PCR_MUX(0x2);      					  /* Set PTD1 to mux 2 [SPI0_SCK] */
+	PORTD->PCR[2] = PORT_PCR_MUX(0x2);      					  /* Set PTD2 to mux 2 [SPI0_MOSI] */
+	PORTD->PCR[3] = PORT_PCR_MUX(0x2);      					  /* Set PTD3 to mux 2 [SPIO_MISO] */
 
-   SPI0->C2 = SPI_C2_MODFEN_MASK;   								  /* Master SS pin acts as slave select output    */
-	
-	 SPI0->BR = (SPI_BR_SPPR(0x02) | SPI_BR_SPR(0x08)); /* Set baud rate prescale divisor to 3 & set baud rate divisor to 64 for baud rate of 15625 hz */
-	
-	 SPI0->C1 = SPI_C1_SPE_MASK;    										/* Enable SPI0 */
+	SPI0->C1 = SPI_C1_MSTR_MASK | SPI_C1_SSOE_MASK;    /* Set SPI0 to Master & SS pin to auto SS */
+
+	SPI0->C2 = SPI_C2_MODFEN_MASK;   								  /* Master SS pin acts as slave select output    */
+
+	SPI0->BR = (SPI_BR_SPPR(0x02) | SPI_BR_SPR(0x08)); /* Set baud rate prescale divisor to 3 & set baud rate divisor to 64 for baud rate of 15625 hz */
+
+	SPI0->C1 = SPI_C1_SPE_MASK;    										/* Enable SPI0 */
 }  
   
 void spi_send(char spiMsg)  
 {  
 	while(!(SPI_S_SPTEF_MASK & SPI0->S))
-  {      
+	{      
 		__asm
-    {
+		{
 			NOP /* While buffer is not empty do nothing */
 		}
-  }  
-  SPI0->D = spiMsg;     /* Write char to SPI */
-}  
+	}  
+	SPI0->D = spiMsg;     /* Write char to SPI */
+}
+
+void spi_recv(char spiMsg)  
+{  
+	while(!(SPI_S_SPTEF_MASK & SPI0->S))
+	{      
+		__asm
+		{
+		NOP /* While buffer is not empty do nothing */
+		}
+	}  
+	spiMsg = SPI0->D;     /* Read char from SPI */
+}
